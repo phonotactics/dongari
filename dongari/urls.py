3f3ca6bc1acd2a2bd50952dd0ce0b043445d1f35
hongjun <vvclsbalam@gmail.com>
2023-06-18 20:47:08 +0900

    django course 27 commit

--- dongari/urls.py
+++ dongari/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accountapp.urls')),
+    path('account/', include('accountapp.urls')),
 ]
