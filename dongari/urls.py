ef90367f5e7554b6f43588dcc74af2f990440538
hongjun <vvclsbalam@gmail.com>
2023-07-28 17:42:40 +0900

    django course 40 commit

--- dongari/urls.py
+++ dongari/urls.py
@@ -22,9 +22,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
+    path('projects/', include('projectapp.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
